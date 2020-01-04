2003be2bfccca8f9061753f55daee62cc65f856f
prabhakarshrestha <prabhakar@optimumfuturist.com>
2020-01-05 02:48:46 +0545

    footer added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
+
+    //menu show hide
     $(".navigator-toggle i").click(function () {
         $(".tuples-navigation").addClass("open");
     });
     $(".close").click(function () {
         $(".tuples-navigation").removeClass("open");
+    });
+
+    // scroll to top
+    $("a[href='#top']").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
     });
 });
